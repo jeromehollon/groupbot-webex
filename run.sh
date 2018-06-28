73fbc289f274667e0eaa9b699a98308c7f25ecea
Jerome Hollon <jerome.hollon.contractor@hilton.com>
2018-06-28 11:18:15 -0500

    Documentation, fixing rooms. Enahcning run script

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-
-# exit if running
-screen -i screenGroupbot
-
 # start daemon
 screen -dmS screenGroupbot bash -c 'CISCOSPARK_ACCESS_TOKEN=$CISCOSPARK_ACCESS_TOKEN nodejs . 2>&1 | tee server.log'
