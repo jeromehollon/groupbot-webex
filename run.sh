7e54d547633195e9cead8a58b71ae125e95e44e6
Jerome Hollon <jerome@hollonconsulting.com>
2018-06-28 15:40:29 +0000

    Updating run script

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-screen -dmS screenGroupbot bash -c "CISCOSPARK_ACCESS_TOKEN=$CISCOSPARK_ACCESS_TOKEN nodejs index.js"
-
+#!/bin/sh
+screen -dmS screenGroupbot bash -c 'CISCOSPARK_ACCESS_TOKEN=$CISCOSPARK_ACCESS_TOKEN nodejs . | tee server.log'
